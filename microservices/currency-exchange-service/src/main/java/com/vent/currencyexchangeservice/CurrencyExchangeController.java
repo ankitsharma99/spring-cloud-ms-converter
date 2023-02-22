630861f2017042088b217069e6ced0f3a62d903c
Ankit Sharma <sharmankit9593@gmail.com>
2023-02-22 16:29:47 +0530

    added micrometer observation dependency

--- microservices/currency-exchange-service/src/main/java/com/vent/currencyexchangeservice/CurrencyExchangeController.java
+++ microservices/currency-exchange-service/src/main/java/com/vent/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.vent.currencyexchangeservice;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +11,16 @@ import java.math.BigDecimal;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CurrencyExchangeController {
+
     private final Environment environment;
     private final CurrencyExchangeRepository currencyExchangeRepository;
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to)  {
+
+        log.info("retrieveExchange value called with {} to {}", from, to);
+
         String port = this.environment.getProperty("local.server.port");
 //        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50), port);
 
